c5f3d29d162fabdb3d89ab67d047c51d266c7669
AndrewNation <andrewpereira.megabyte@gmail.com>
2024-10-16 10:32:07 -0300

    update home

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'AndrewNation',
-  tagline: 'Um programador front-end com curiosidade infinita',
+  tagline: 'Um programador front-end com curiosidade infinita, movido pelas ideias e criatividade',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
