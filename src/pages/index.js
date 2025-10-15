16680e5f760146497a00cbdf156ca8655873ba92
AndrewNation <andrewpereira.megabyte@gmail.com>
2025-10-15 21:43:03 +0000

    add a buy me a coffee component

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,7 @@ function HomepageHeader() {
           >
             Meus livros e contos
           </a>
+          <a href="https://www.buymeacoffee.com/andrewnation"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=andrewnation&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>
         </div>
       </div>
     </header>
